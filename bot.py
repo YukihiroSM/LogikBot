2edf54f7fb80313b2cb091250290bb8b70c7dd83
Yuko <yukihirosm@gmail.com>
2020-10-29 23:42:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ext
 import logging
 import sqlite3
@@ -11,13 +15,11 @@ updater = ext.Updater(token=TOKEN, use_context=True)
 
 dispatcher = updater.dispatcher
 
-GR_ID, USERNAME, LOGIKS = range(3)
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Привет, это Логик! На данный момент я мне... кхм... Подкручивают гайки. Скоро меня закончат ремонтировать и мы поработаем снова. ')
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Привет, это Логик! На данный момент я мне... кхм... Подкручивают гайки. Скоро меня закончат ремонтировать и мы поработаем снова.')
 
 start_handler = ext.CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-updater.dispatcher.add_handler(ext.CallbackQueryHandler(button))
 updater.start_polling()
