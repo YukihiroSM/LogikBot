55424c0b6fdfd0fa2dd2c61cba51999bf7b91223
Yuko <yukihirosm@gmail.com>
2020-10-30 00:02:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,24 +2,12 @@
 # -*- coding: utf-8 -*-
 # This program is dedicated to the public domain under the CC0 license.
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ext
-import logging
-import sqlite3
+import telebot
 
-DB = 'BOT_DATABASE.db'
+bot = telebot.TeleBot('1389262284:AAEy-EyDc3XKB9DXIJ-yXE7s9Or_eEDOlgs')
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-TOKEN = "1389262284:AAEy-EyDc3XKB9DXIJ-yXE7s9Or_eEDOlgs"
-updater = ext.Updater(token=TOKEN, use_context=True)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет. Это Логик! Сейчас меня немного чинят. Давай попробуем попозже? Если что - пиши моему создателю: @notProgrammerYuk0")
 
-dispatcher = updater.dispatcher
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Привет, это Логик! На данный момент я мне... кхм... Подкручивают гайки. Скоро меня закончат ремонтировать и мы поработаем снова.')
-
-start_handler = ext.CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-updater.start_polling()
+bot.polling()
